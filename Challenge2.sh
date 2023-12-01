f6f2e6ede5acbfde8e7ff3dc8f47b2e0d4e4afbb
JoeGutmann <Joe.Gutmann@gmail.com>
2023-11-30 19:53:35 -0600

    Challenge Complete

--- Challenge2.sh
+++ Challenge2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script Name:                  Ops Challenge - Append; Date and Time
+# Script Name:                  Ops Challenge - File Permissions
 # Author:                       Joe Gutmann
 # Date of latest revision:      29NOV2023
 # Purpose:                      Ops Challenge 301: Class 03
@@ -22,8 +22,10 @@ echo "The directory "$DN" has been created"
 
 echo "Please enter in the permissions numer you want, in this case enter in 777 to perform a chmod 777"
 read PN
-chmod 777 "$DN"
-
+# chmod 777 "$DN"
+chmod -R $PN $DN 
 # navigate to the directory // print to the screen the directory contents and the new permissions settings of everything in the dir
 cd "$DN"
 ls -la
+
+#watch class video 11.30.23 if you need to review and fix the assignment. This here is not exactly what was uploaded to canvas. or github as of 1158 11.30.23
